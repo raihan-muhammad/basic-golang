3162fba3d6dd3b062dcf8b1c50d16d93cff01514
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-21 14:37:50 +0700

    add example of varible in golang

---
+++ 01-variable/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var age int8 = 23
+func main(){
+	name := "raihan"
+	var isMerried bool = false
+	fmt.Printf("age: %v isMerried: %v name: %s\n", age, isMerried, name)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module basic-golang
+
+go 1.21.6
