1e46bbd9ef1ba3428e1ee715a636ad65c629411e
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-21 15:05:27 +0700

    feat: add example of slices, struct and map

--- 01-variable/main.go
+++ 01-variable/main.go
@@ -2,9 +2,37 @@ package main
 
 import "fmt"
 
-var age int8 = 23
+
+type biodata struct {
+	name string
+	age int8
+	hobby []string
+}
+
+
 func main(){
+	// example of variables
 	name := "raihan"
+	var age int8 = 23
 	var isMerried bool = false
 	fmt.Printf("age: %v isMerried: %v name: %s\n", age, isMerried, name)
+
+	// example of slices
+	hobby := []string { "mancing", "ngoding"}
+	fmt.Println(hobby[1])
+
+	// example of struct
+	person := biodata {
+		name: "raihan",
+		age: 23,
+		hobby: []string { "mancing", "ngoding"},
+	}
+	fmt.Println(person.hobby[0]);
+
+	// example of map
+	school := map[string]string{
+		"name": "Smk mutuharjo",
+		"classes": "100",
+	}
+	fmt.Println(school["classes"])
 }
